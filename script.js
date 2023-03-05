be74e205bde85d748bf79036b13a731378a22f10
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-04 19:00:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+let Kelvin = 0;
+let Celsius = 273;
+
+let celsius = Kelvin - Celsius;
+
+let fahrenheit = celsius * (9/5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+
+
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+// Newton
+
+Celsius = 100;
+
+
+let newton = Celsius * (33/100);
+
+newton = Math.floor(newton);
+
+console.log(newton);
